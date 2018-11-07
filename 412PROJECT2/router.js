b48ad03740d9670a4e40b5a4cedaed0276f67bd9
adrbell18 <43450306+adrbell18@users.noreply.github.com>
2018-11-07 02:27:41 -0500

    Update router.js

--- 412PROJECT2/router.js
+++ 412PROJECT2/router.js
@@ -2,3 +2,5 @@ app.route('/forgotpasswordResponse')
 .post(userCtrl.forgotpasswordResponse);
 app.route('/reset/:token')  
 .get(Resetpassword.resetpasswordResponse);
+app.route('/reset/:token')  
+  .post(setpassword.setpasswordResponsemail);
